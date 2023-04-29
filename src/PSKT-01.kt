7bafd7315998bc41bc9b531eac84f42193ce8f7c
nhd0918 <nhd0918@naver.com>
2023-04-29 15:59:59 +0900

    코틀린 27889번 백준 문제 커밋

---
+++ src/PSKT-01.kt
@@ -0,0 +1,10 @@
+
+fun main(args: Array<String> ) {
+    val name = readln()
+    when (name) {
+        "NLCS" -> println("North London Collegiate School")
+        "BHA"  -> println("Branksome Hall Asia")
+        "KIS"  -> println("Korea International School")
+        "SJA"  -> println("St. Johnsbury Academy")
+    }
+}
