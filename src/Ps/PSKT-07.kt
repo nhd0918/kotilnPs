1240067a0cba089161fd13f477c1dc023abe8be2
nhd0918 <nhd0918@naver.com>
2023-05-13 05:59:07 +0900

    코틀린 문법 공부

--- src/Ps/PSKT-07.kt
+++ src/Ps/PSKT-07.kt
@@ -9,17 +9,18 @@ import kotlin.math.max
 * 알파벳 대문자를 10부터 시작하여 A ~ Z까지의 수를
 * * * */
 fun main() {
-    val nums = listOf<Int>(2,3,4,5)
-    val t = 9
+    val nums = listOf<Int>(3,3)
+    val target = 6
     val arr = IntArray(2)
 
-    for(i in nums.indices) {
-        if( nums.size - 1 != i) {
-            if( t == nums[i] + nums[i + 1] ) {
+    loop@for(i in 0..nums.size) {
+        for(j in i + 1..nums.size - 1) {
+            if( nums[i] + nums[j] == target ) {
                 arr[0] = i
-                arr[1] = i + 1
+                arr[1] = j
+                break@loop
             }
         }
     }
-
+    println("${arr[0]} ${arr[1]}")
 }
